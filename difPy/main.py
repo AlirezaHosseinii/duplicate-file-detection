8e6b416e4e8a6e106b9c3ca93ea18b90415f1bc4
Alireza Hosseini <alireza.hoseini20031382@gmail.com>
2022-09-29 18:06:51 +0330

    Getting duplicated photos of a path

--- difPy/main.py
+++ difPy/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from difPy import dif
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+path = str(input("Enter the path: "))
 
+search_for_duplicated_photos = dif(path)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+duplicated_photos = search_for_duplicated_photos.result
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(duplicated_photos)
